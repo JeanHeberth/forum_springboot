dedc3a7c107d16740921cfbe7157b0319f29e34a
Jean Heberth <jeanheberth19@gmail.com>
2022-06-29 15:44:57 -0300

    Foi implementado a classe TopicoForm

--- src/main/java/br/com/jeanheberth/forum/controller/TopicoController.java
+++ src/main/java/br/com/jeanheberth/forum/controller/TopicoController.java
@@ -1,23 +1,52 @@
 package br.com.jeanheberth.forum.controller;
 
 
+import br.com.jeanheberth.forum.controller.forum.TopicoForm;
 import br.com.jeanheberth.forum.dto.TopicoDto;
-import br.com.jeanheberth.forum.modelo.Curso;
 import br.com.jeanheberth.forum.modelo.Topico;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.jeanheberth.forum.repository.CursoRepository;
+import br.com.jeanheberth.forum.repository.TopicoRepository;
+import com.sun.jndi.toolkit.url.Uri;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.Arrays;
+import java.net.URI;
 import java.util.List;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicoController {
 
-    @RequestMapping("/topicos")
-      public List<TopicoDto> lista(){
-        Topico topico = new Topico("Dúvida", "Duvida Spring", new Curso("Spring", "Programação"));
-        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+    @Autowired
+    private TopicoRepository topicoRepository;
+
+    @Autowired
+    private CursoRepository cursoRepository;
+
+
+    /* Método para listar curso por nome */
+    @GetMapping
+    public List<TopicoDto> lista(String nomeCurso) {
+        if (nomeCurso == null) {
+            List<Topico> topicos = topicoRepository.findAll();
+            return TopicoDto.converter(topicos);
+        } else {
+            List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+            return TopicoDto.converter(topicos);
+        }
     }
+
+    /* Método para cadastrar curso */
+    @PostMapping
+    public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm topicoForm, UriComponentsBuilder uriComponentsBuilder) {
+        Topico topico = topicoForm.converter(cursoRepository);
+        topicoRepository.save(topico);
+
+        URI uri = uriComponentsBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+        return ResponseEntity.created(uri).body(new TopicoDto(topico));
+    }
+
 }
